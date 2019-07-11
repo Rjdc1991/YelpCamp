56140071856b6c55a10a7aad0b58f7b364a21cc8
rjdc1991 <rjdc1991@icloud.com>
2019-07-11 05:33:33 +0000

    css fx btn style

---
+++ public/stylesheets/yelp.js
@@ -0,0 +1,14 @@
+//document.querySelectorAll(".btn-primary").onclick= function () {
+				
+		//document.querySelectorAll(".btn-primary").style.border ="none";
+												  					  
+		//};("
+
+document.getElementsByClassName("btn-primary").onclick=function(){
+	
+	document.getElementsByClassName("btn-primary").style.background="black";
+};
+
+
+
+
